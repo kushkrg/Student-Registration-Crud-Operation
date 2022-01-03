93228f125dcaa1516825aaeacff498ce95ddbf36
Lavkush kumar <65346284+kushkrg@users.noreply.github.com>
2022-01-02 20:24:23 -0800

    Edit image problem
    When we update the name or any other information except the image then image is automatically removed. 
    
    now fixed this problem.

--- edit.php
+++ edit.php
@@ -3,6 +3,28 @@ include('db.php');
 
 $id = $_GET['id'];
 
+//Fetching privious image to update
+if(isset($_GET['id'])){
+    $edit_id = $_GET['id'];
+    $edit_query = "SELECT * FROM card_activation WHERE id = $edit_id";
+    $edit_query_run = mysqli_query($con, $edit_query);
+    if(mysqli_num_rows($edit_query_run) > 0){
+        $edit_row = mysqli_fetch_array($edit_query_run);
+      
+        $e_image = $edit_row['image'];
+     
+    }
+    else{
+        // header('location: index.php');
+        echo "Error1";
+    }
+}
+else{
+    // header("location: index.php");
+    echo "Error2";
+}
+
+//Data Updating
 if(isset($_POST['submit']))
 {
 	$u_card = $_POST['card_no'];
@@ -25,19 +47,21 @@ if(isset($_POST['submit']))
 	
 	$msg = "";
 	$image = $_FILES['image']['name'];
+	if(empty($image)){
+	    $image = $e_image;
+	}
 	$target = "upload_images/".basename($image);
 
-	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-  		$msg = "Image uploaded successfully";
-  	}else{
-  		$msg = "Failed to upload image";
-  	}
 
 	$update = "UPDATE card_activation SET u_card='$u_card', u_f_name = '$u_f_name', u_l_name = '$u_l_name', u_father = '$u_father', u_mother = '$u_mother', u_aadhar = '$u_aadhar', u_birthday = '$u_birthday', u_gender = '$u_gender', u_email = '$u_email', u_phone = '$u_phone', u_state = '$u_state', u_dist = '$u_dist', u_village = '$u_village', u_police = '$u_police', u_pincode = '$u_pincode', u_family = '$u_family', staff_id = '$u_staff_id', image = '$image' WHERE id=$id ";
 	$run_update = mysqli_query($con,$update);
 
 	if($run_update){
-		header('location:index.php');
+           move_uploaded_file($_FILES['image']['tmp_name'], $target);
+           echo "<script>
+            alert('Success! Data has been successfully updated!');
+            window.location.href='index.php';
+            </script>";
 	}else{
 		echo "Data not update";
 	}
